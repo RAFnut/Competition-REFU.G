a485d235c431cf1486b3f2c3a13060a2ca127e25
NikolaKlipa <klipa.nikola@gmail.com>
2015-11-15 00:40:07 +0100

    User and edit should be working

---
+++ src/AppBundle/Form/EditUserType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class EditUserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('password', 'repeated', array(
+                'type' => 'password',
+                'invalid_message' => 'The password fields must match.',
+                'options' => array('attr' => array('class' => 'password-field')),
+                'required' => false,
+                'first_options'  => array('label' => 'New Password'),
+                'second_options' => array('label' => 'Repeat Password'),
+            ))
+            ->add('full_name', 'text')
+            ->add('email', 'email')
+            ->add('number', 'text')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'appbundle_user';
+    }
+}
