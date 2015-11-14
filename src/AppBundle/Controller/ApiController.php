794c3657c68519a0f15129f888ede1e5f048fa7b
Andrej Karadzic <karadza3a@yahoo.com>
2015-11-14 22:02:57 +0100

    controllers

---
+++ src/AppBundle/Controller/ApiController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+/**
+ * @Route("/app/api", name="api")
+ */
+class ApiController extends Controller
+{
+    private function serialize($object)
+    {
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+        $serializer = new Serializer($normalizers, $encoders);
+        $jsonContent = $serializer->serialize($object, 'json');
+        return $jsonContent;
+    }
+
+    private function deserialize($json, $entityName)
+    {
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+        $serializer = new Serializer($normalizers, $encoders);
+        $object = $serializer->deserialize($json, $entityName, 'json');
+        return $object;
+    }
+
+    /**
+     * @Route("/update-status", name="updateStatusApi")
+     */
+    public function updateStatusApiAction(Request $request)
+    {
+        $json = $request->request->get('status');
+        var_dump($json);
+        $status = $this->deserialize($json, 'AppBundle\Entity\Status');
+        var_dump($status);
+        return new JSONResponse('ok');
+    }
+
+}
