cf369813b6889ea93be6019f8dd54e7512bff7a7
Filip Danic <filipdanic@yahoo.com>
2015-11-14 23:10:31 +0100

    update-status geolocation bullshit works great!

--- src/AppBundle/Resources/public/js/app/check_location.js
+++ src/AppBundle/Resources/public/js/app/check_location.js
@@ -1,26 +1,45 @@
 var pinStringContainer = $(".geo-location-current-pin-string");
 var latitudeCord;
 var longitudeCord;
+
+/*
+  * Trazi permission od usera
+*/
 function getLocationFromUser() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
-
     }
     else {
         pinStringContainer.append("Geolocation is not supported by this browser.");
     }
 }
 
+/*
+  * Zapoicnje reverseLookUp
+*/
+
 function showPosition(position) {
     latitudeCord = position.coords.latitude;
     longitudeCord = position.coords.longitude;
-    pinStringContainer.append("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
     var reverseLookUp = "http://maps.googleapis.com/maps/api/geocode/json?latlng=";
     reverseLookUp += position.coords.latitude+","+position.coords.longitude;
+    /*AJAX GET prkeo mog frameworka :3 */
     _R.sendGET(reverseLookUp, reverseLookUpByCoords, _R.log);
 }
 
 
+function showPositionAlt(position) {
+    latitudeCord = position.coords.latitude;
+    longitudeCord = position.coords.longitude;
+    var reverseLookUp = "http://maps.googleapis.com/maps/api/geocode/json?latlng=";
+    reverseLookUp += position.coords.latitude+","+position.coords.longitude;
+    /*AJAX GET prkeo mog frameworka :3 */
+    _R.sendGET(reverseLookUp, reverseLookUpByCoordsAlt, _R.log);
+}
+
+/*
+  * dobijeni google json pretvara u citljivu adresicu
+*/
 function decodeLocationArray(results){
   city = "";
   country = "";
@@ -36,14 +55,60 @@ function decodeLocationArray(results){
 
       }
       if (city != "" && country != ""){
-        return city+", "+country;
-        break;
+        tempstr = city+", "+country;
+        return tempstr;
       }
     }
   }
-  return city+", "+country;
+  tempstr = city+", "+country;
+  return tempstr;
 }
 
+
+function initGoogleMap() {
+    // More info see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    var mapOptions = {
+        zoom: 11,
+        center: new google.maps.LatLng(latitudeCord, longitudeCord),
+        // Style for Google Maps
+        styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
+    };
+
+    // Get all html elements for map
+    var mapElement = document.getElementById('checkUserLocationMap');
+
+    // Create the Google Map using elements
+    var map = new google.maps.Map(mapElement, mapOptions);
+    var myLatLng = {lat: latitudeCord, lng: longitudeCord};
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      draggable: true,
+      title: 'Nearest'
+    });
+    marker.setMap(map);
+    google.maps.event.addListener(marker, 'dragend', function (event) {
+        var position = {
+          coords : {"latitude": 0,"longitud":0}
+        };
+        position.coords.latitude = event.latLng.lat();
+        position.coords.longitude = event.latLng.lng();
+        showPositionAlt(position);
+    });
+}
+
+
+
+/*
+  * dobija 'near' adresicu i radi //nesto sa njom
+*/
 function reverseLookUpByCoords(results){
-  console.log(decodeLocationArray(results));
+  nearest = decodeLocationArray(results);
+  $('.geo-location-current-pin-string').val(nearest);
+  initGoogleMap();
+}
+
+function reverseLookUpByCoordsAlt(results){
+  nearest = decodeLocationArray(results);
+  $('.geo-location-current-pin-string').val(nearest);
 }
